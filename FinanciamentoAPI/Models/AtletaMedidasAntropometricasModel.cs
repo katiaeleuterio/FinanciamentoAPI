f0d9542917401f7a18e814a10710982b29713e4d
Rodrigo Celeghini <rodrigo@celeghini.com>
2022-06-04 11:56:49 -0300

    Inclusão de comentários na classe AtletaMedidasAntropometricasModel

--- FinanciamentoAPI/Models/AtletaMedidasAntropometricasModel.cs
+++ FinanciamentoAPI/Models/AtletaMedidasAntropometricasModel.cs
@@ -2,17 +2,21 @@
 
 namespace BikeFit.Models
 {
+    //Medidas antropométricas do atleta
     public class AtletaMedidasAntropometricasModel
     {
         [Key]
         public int Id { get; set; }
 
+        //fk horiunda da tabela AtletaDadosPessoais
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         public int IdAtleta { get; set; }
 
+        //Velor em cm
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         public decimal Cavalo { get; set; }
 
+        //Valor em cm
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         public decimal Antebraco { get; set; }
 
